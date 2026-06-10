be318c3ae7d5d0d7643f27505453a7ea329c3d86
matthewckrueger <matthew@kdkruegerfarms.com>
2026-06-10 18:23:46 -0500

    add attractions data

---
+++ data/attractions.js
@@ -0,0 +1,22 @@
+const attractionsData = [
+  {
+    title: "Corn Maze",
+    description: "Explore our large themed corn maze carved into the Red River Valley.",
+    icon: "fa-solid fa-seedling"
+  },
+  {
+    title: "Giant Slide",
+    description: "A favorite for kids and families every fall.",
+    icon: "fa-solid fa-person-running"
+  },
+  {
+    title: "Grain Wagon Train",
+    description: "Hop aboard for a fun ride around the farm.",
+    icon: "fa-solid fa-tractor"
+  },
+  {
+    title: "Pumpkin Patch",
+    description: "Pick pumpkins and take fall family photos.",
+    icon: "fa-solid fa-pumpkin"
+  }
+];
