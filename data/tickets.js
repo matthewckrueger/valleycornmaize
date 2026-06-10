587a66f609e7b9b7b4800bc75c8cf0e565e5aaaa
matthewckrueger <matthew@kdkruegerfarms.com>
2026-06-10 18:44:20 -0500

    Update tickets.js

--- data/tickets.js
+++ data/tickets.js
@@ -1,17 +1,9 @@
 const ticketsData = {
   dayPass: {
-    type: "Day Admission",
-    price: "$12.50",
-    description: "General admission to Valley Corn Maize.",
-    buttonText: "Buy Day Tickets",
     buttonLink: "https://valleycornmaize.square.site/"
   },
 
   seasonPass: {
-    type: "Season Pass",
-    price: "$28.00",
-    description: "Unlimited visits during the regular season.",
-    buttonText: "Buy Season Pass",
     buttonLink: "https://valleycornmaize.square.site/"
   }
 };
