94d2d4003efbb8b29e601406107bf849c9e4fc7c
matthewckrueger <matthew@kdkruegerfarms.com>
2026-06-10 21:55:43 -0500

    Create smooth-scroll.js

---
+++ js/smooth-scroll.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (e) {
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (!target) return;
+
+    e.preventDefault();
+
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+});
